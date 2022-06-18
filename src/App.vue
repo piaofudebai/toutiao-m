ae0925b2c7ab7a571b77ea6e8d21dbdb7f843650
piaofudebai <248278307@qq.com>
2022-06-18 18:54:01 +0800

    项目初始化-引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,17 @@
   <div id="app">
     <!-- 路由的出口 -->
     <h1>阿江的新闻头条</h1>
+    <i class="toutiao toutiao-shipin"></i>
+    <van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+
+<van-cell-group>
+  <van-cell title="单元格" value="内容" />
+  <van-cell title="单元格" value="内容" label="描述信息" />
+</van-cell-group>
     <router-view/>
   </div>
 </template>
